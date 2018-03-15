592f7222782c62745676db6919bb3aca70942b05
nurlingo <nursultan@outlook.com>
2018-03-15 12:43:46 +0600

    ectelique copy pasted

---
+++ post_form.php
@@ -0,0 +1,54 @@
+<?php
+if( ini_get('allow_url_fopen') ) {
+        echo 'allow_url_fopen is enabled. file_get_contents should work well';
+    } else {
+        echo 'allow_url_fopen is disabled. file_get_contents would not work';
+    }
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if (!empty($_POST['name']) && !empty($_POST['phone'])){
+  if (isset($_POST['name'])) {
+    if (!empty($_POST['name'])){
+  $name = strip_tags($_POST['name']);
+  $nameFieldset = "Имя пославшего: ";
+  }
+}
+
+if (isset($_POST['phone'])) {
+  if (!empty($_POST['phone'])){
+  $phone = strip_tags($_POST['phone']);
+  $phoneFieldset = "Телефон: ";
+  }
+}
+if (isset($_POST['theme'])) {
+  if (!empty($_POST['theme'])){
+  $theme = strip_tags($_POST['theme']);
+  $themeFieldset = "Тема: ";
+  }
+}
+$token = "567654300:AAEbiO3kpQ_vMC1E-YqjSX8OtnsE8BWASLI";
+$chat_id = "-294056907";
+$arr = array(
+  $nameFieldset => $name,
+  $phoneFieldset => $phone,
+  $themeFieldset => $theme
+);
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+if ($sendToTelegram) {
+
+  echo '<p class="success">Спасибо за отправку вашего сообщения!</p>';
+    return true;
+} else {
+  echo '<p class="fail"><b>Ошибка. Сообщение не отправлено!</b></p>';
+}
+} else {
+  echo '<p class="fail">Ошибка. Вы заполнили не все обязательные поля!</p>';
+}
+} else {
+header ("Location: /");
+}
+
+?>
